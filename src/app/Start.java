c66bb8a203a0e5ea9ec7858d4c968f8b6bc8c858
joestr <strasser999@gmail.com>
2018-03-14 15:04:17 +0100

    Correct spelling

--- src/app/Start.java
+++ src/app/Start.java
@@ -5,7 +5,7 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Java-Git-World!");
-		// System.out.println("GoodBye Java-Git-World!");
+		System.out.println("Goodbye Java-Git-World!");
 	}
 
 }
